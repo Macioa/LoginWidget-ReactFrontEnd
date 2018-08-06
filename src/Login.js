dbf5ed6e5594d5be4e3ec77cef599d84441f6a5c
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-06 11:45:49 -0500

    validated username

--- src/Login.js
+++ src/Login.js
@@ -5,18 +5,30 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import Button from '@material-ui/core/Button';
 
 class Login extends Component {
-  state={expanded:false}
-  toggleExpanded=(e)=>{
-    console.log(this.state.expanded)
-    this.setState({expanded:!this.state.expanded})}
+  state={expanded:false, username:null, password:null}
+
+  toggleExpanded=(e)=>{this.setState({expanded:!this.state.expanded})}
+  
+  onFormChange=(e)=>{
+    if (e.target.name==='username') 
+       var validated = e.target.value.replace(/[^a-z0-9]/gi,''); 
+    else { var validated = e.target.value; }
+    console.log(validated)
+    this.setState({[e.target.name]:validated})
+  }
+
+  handleSubmit=(e)=>{
+    console.log('submit')
+  }
+
   render() {
     return (
       <div className="Login" style={{borderStyle:'solid', borderRadius:'15px', borderWidth:"1px", width:150, textAlign:'center'}}>
         <Button onClick={this.toggleExpanded} style={{borderRadius:"15px"}}>Login</Button>
         <ExpansionPanel expanded={this.state.expanded} style={{width:"125px", margin:"10px auto 10px", textAlign:'center', borderRadius:'10px'}}>
-            <form style={{margin:'5px'}}>
-              <input type="text" name="username" placeholder="Username"/><br/>
-              <input type="password" name="password" placeholder="Password"/><br/>
+            <form style={{margin:'5px'}} onSubmit={this.handleSubmit}>
+              <input type="text" name="username" placeholder="Username" onChange={this.onFormChange} value={this.state.username}/><br/>
+              <input type="password" name="password" placeholder="Password" onChange={this.onFormChange} value={this.state.password}/><br/>
               <input type="submit" value="Submit"/>
             </form>
         </ExpansionPanel>
