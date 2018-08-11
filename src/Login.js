c24806099d77e6706d424082c545fca58399059a
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-10 19:00:53 -0500

    a

--- src/Login.js
+++ src/Login.js
@@ -28,7 +28,7 @@ class Login extends Component {
     const response = await fetch(`${this.state.loginserver}${path}`, {
       method: 'POST',
       credentials: 'include',
-      body: myS="asasdfasfdasdfs",
+      body: myS="asasdfasfdasdfs:asfdasfd",
       data: "asdfsadf",
       headers: { 'content-type':'application/x-www-form-urlencoded; charset=UTF-8',  }
     })
